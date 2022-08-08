4bfb41fddcb216bd9116605e0f21ee16fc7fa622
cotel557 <cotel557@gmail.com>
2022-08-08 17:27:38 +1200

    Make buttons change display

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ function div(a, b) {
 function operate(a, b, operator) {
   return operator(a, b);
 }
+
+let displayValue = "";
+buttons = document.querySelectorAll('.key');
+display = document.querySelector('#display');
+
+function clickButton(e) {
+  const char = e.target.textContent;
+  const num = parseInt(char);
+  if (!isNaN(num)) {
+    displayValue = displayValue + num;
+  } else if (char === "C") {
+    displayValue = "";
+  }
+  display.textContent = displayValue;
+}
+
+buttons.forEach(b => b.addEventListener('click', clickButton));
