27b2abda19eee71a14aa00981ef4d3add8bb61bd
cotel557 <cotel557@gmail.com>
2022-08-13 21:00:30 +1200

    Refactor code

--- script.js
+++ script.js
@@ -1,36 +1,83 @@
-function add(a, b) {
-  return a + b;
+function operate(operator, a, b) {
+  const opToFunc = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b
+  }
+  return opToFunc[operator](a, b);
 }
 
-function sub(a, b) {
-  return a - b;
-}
-
-function mul(a, b) {
-  return a * b;
-}
-
-function div(a, b) {
-  return a / b;
-}
-
-function operate(a, b, operator) {
-  return operator(a, b);
-}
-
-let displayValue = "";
-buttons = document.querySelectorAll('.key');
-display = document.querySelector('#display');
+let calcState = {
+  displayText: "",
+  chosenOp: "",
+  prevText: "",
+  lastKind: "clear"
+};
 
 function clickButton(e) {
   const char = e.target.textContent;
-  const num = parseInt(char);
-  if (!isNaN(num)) {
-    displayValue = displayValue + num;
-  } else if (char === "C") {
-    displayValue = "";
+  const kind = e.target.getAttribute('data-kind');
+  const kindToFunc = {
+    "num": chooseNum,
+    "clear": chooseClear,
+    "equals": chooseEquals,
+    "op": chooseOp
   }
-  display.textContent = displayValue;
+  const func = kindToFunc[kind];
+
+  func(calcState, char);
+
+  updateDisplay(calcState);
 }
 
+function chooseNum(c, char) {
+  if (c.lastKind === "num") {
+    c.displayText = c.displayText + char;
+  } else {
+    c.displayText = "" + char;
+  }
+  c.lastKind = "num";
+}
+
+function chooseClear(c, char) {
+  c.displayText = "";
+  c.chosenOp = "";
+  c.prevText = "";
+  c.lastKind = "clear";
+}
+
+function chooseEquals(c, char) {
+  if (c.chosenOp && c.lastKind === "num") {
+    c.prevText = "" + operate(c.chosenOp, parseInt(c.prevText), parseInt(c.displayText));
+    c.displayText = c.prevText;
+    c.chosenOp = "";
+    c.lastKind = "equals";
+  }
+}
+
+function chooseOp(c, char) {
+  if (c.displayText) {
+    if (!c.chosenOp) {
+      c.prevText = c.displayText;
+      c.chosenOp = char;
+      c.lastKind = "op";
+    } else if (c.lastKind === "num") {
+      c.prevText = "" + operate(c.chosenOp, parseInt(c.prevText), parseInt(c.displayText));
+      c.displayText = c.prevText;
+      c.chosenOp = char;
+      c.lastKind = "op";
+    } else if (c.lastKind === "op") {
+      c.chosenOp = char;
+      c.lastKind = "op";
+    }
+  }
+}
+
+function updateDisplay(c) {
+  display.textContent = c.displayText;
+}
+
+buttons = document.querySelectorAll('.key');
+display = document.querySelector('#display');
 buttons.forEach(b => b.addEventListener('click', clickButton));
